28fd05919fb4e8afae3282ee613170c1c652c2f3
Bruno_Neves <nevesbruno814@gmail.com>
2025-01-20 22:36:23 -0400

    Game seed

--- src/main/java/com/brunoneves/dslist/entities/Game.java
+++ src/main/java/com/brunoneves/dslist/entities/Game.java
@@ -1,6 +1,9 @@
 package com.brunoneves.dslist.entities;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.DialectOverride;
+import org.hibernate.annotations.Instantiator;
+import org.springframework.core.annotation.Order;
 
 import java.util.Objects;
 
@@ -15,19 +18,26 @@ public class Game {
 
     @Column(name = "game_year")
     private Integer year;
+
+
+
+    private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
+        this.genre = genre;
         this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
@@ -57,6 +67,14 @@ public class Game {
 
     public void setYear(Integer year) {
         this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 
     public String getPlatforms() {
